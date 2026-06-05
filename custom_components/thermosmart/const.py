b31dec68fe2aa7dd1286acdda7785187a32115b3
Mikasmarthome <278800977+Mikasmarthome@users.noreply.github.com>
2026-06-05 11:41:42 +0200

    chore: bump version to v1.0.1-beta.2

--- custom_components/thermosmart/const.py
+++ custom_components/thermosmart/const.py
@@ -1,7 +1,7 @@
 """Constants for the ThermoSmart integration."""
 
 DOMAIN = "thermosmart"
-VERSION = "1.0.1-beta.1"
+VERSION = "1.0.1-beta.2"
 
 # Config entry keys
 CONF_CLIMATE_ENTITIES = "climate_entities"

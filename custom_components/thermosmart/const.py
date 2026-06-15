e787d15514ea3636cb7c87d251f0471ef09ad9ef
Mikasmarthome <278800977+Mikasmarthome@users.noreply.github.com>
2026-06-15 19:06:32 +0200

    chore: prepare v1.1.0-rc.3 release candidate

--- custom_components/thermosmart/const.py
+++ custom_components/thermosmart/const.py
@@ -1,7 +1,7 @@
 """Constants for the ThermoSmart integration."""
 
 DOMAIN = "thermosmart"
-VERSION = "1.1.0-rc.2"
+VERSION = "1.1.0-rc.3"
 
 # Config entry keys
 CONF_CLIMATE_ENTITIES = "climate_entities"
